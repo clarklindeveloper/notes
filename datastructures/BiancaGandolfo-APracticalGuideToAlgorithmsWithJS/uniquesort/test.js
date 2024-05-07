35f855b265357952b1828131d5204d013f4788b1
clarklindev <clarklindev@gmail.com>
2024-05-07 13:26:17 +0800

    moved to own repos

--- datastructures/BiancaGandolfo-APracticalGuideToAlgorithmsWithJS/uniquesort/test.js
+++
@@ -1,9 +0,0 @@
-const S = require('./index');
-const uniqueSort = S.uniqueSort;
-
-describe('Unique sort', () => {
-  test('no repeated entries', () => {
-    const result = uniqueSort([3,2,1,'yo',1,1,2,3,'pool']);
-    expect(result).toEqual([1,2,3,'pool','yo']);
-  });
-});
